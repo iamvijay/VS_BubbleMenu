d251ef1b89b68334df3bfcfbba7d17385e8355cb
Vijay Subramaniyam <vijay.subramaniyam@nowfloats.com>
2015-04-11 21:30:25 +0530

    Initial Commit

---
+++ VSAnimatedMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSAnimatedMenu
+//
+//  Created by Vijay subramaniyam on 11/04/15.
+//  Copyright (c) 2015 vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSAnimatedMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSAnimatedMenu
+//
+//  Created by Vijay subramaniyam on 11/04/15.
+//  Copyright (c) 2015 vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
